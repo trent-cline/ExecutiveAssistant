2e1f41a16c37290da4de2376404222448182cec2
Trent <trentcline@live.com>
2025-01-11 18:36:48 -0700

    4

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-
 	kit: {
-		adapter: adapter({
-			runtime: 'nodejs20.x'
-		})
-	}
+		adapter: adapter()
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
